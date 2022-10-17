963a58b899b32cc3792dac5e4fdeebfa065411ed
CherryKitten <git@cherrykitten.dev>
2022-10-17 11:13:33 +0200

    Add markdown previewer certification project

---
+++ 3-frontend-dev-libraries/2-markdown-previewer/src/App.js
@@ -0,0 +1,82 @@
+import './App.css';
+import Markdown from "marked-react"
+import React from "react";
+import defaultPreview from "./defaultPreview";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: defaultPreview,
+            preview: defaultPreview,
+            fullscreenEditor: false,
+            fullscreenPreview: false,
+        };
+    }
+
+    updateText = (event) => {
+        this.setState({input: event.target.value, preview: event.target.value})
+    }
+
+    setFullscreen = (event, target) => {
+        target === 'editor'
+            ? this.state.fullscreenEditor === false
+                ? this.setState({fullscreenEditor: true, fullscreenPreview: false})
+                : this.setState({fullscreenEditor: false})
+            : this.state.fullscreenPreview === false
+                ? this.setState({fullscreenPreview: true, fullscreenEditor: false})
+                : this.setState({fullscreenPreview: false})
+    }
+
+    render() {
+        const editorStyle = {
+            width: this.state.fullscreenEditor ? '80%' : '20%',
+            display: this.state.fullscreenPreview ? 'none' : 'initial',
+            opacity: this.state.fullscreenPreview ? '0' : '100',
+            height: '80%',
+            position: 'relative',
+
+        }
+        const previewStyle = {
+            width: this.state.fullscreenPreview ? '80%' : '40%',
+            display: this.state.fullscreenEditor ? 'none' : 'initial',
+            opacity: this.state.fullscreenEditor ? '0' : '100',
+            height: '80%',
+            position: 'relative',
+        }
+        return (
+            <div className={"App"} style={{position: 'relative'}}>
+                <div style={editorStyle}>
+                    <button
+                        id={"setFullscreenEditor"}
+                        style={{position: "absolute", right: 5, top: 5}}
+                        onClick={ (e) => this.setFullscreen(e, 'editor') }
+                    >
+                        <i className="fa-sharp fa-solid fa-expand"></i>
+                    </button>
+                    <textarea
+                        style={{resize: 'none', height: '100%', width: '100%'}}
+                        id="editor"
+                        onChange={this.updateText}
+                        value={this.state.input}
+                    ></textarea>
+                </div>
+                <div style={previewStyle}>
+                    <button id={"setFullscreenPreview"}
+                            style={{position: "absolute", right: 5, top: 5}}
+                            onClick={ (e) => this.setFullscreen(e, 'preview') }
+                    >
+                        <i className="fa-sharp fa-solid fa-expand"></i>
+                    </button>
+                    <div id={'preview'} style={{maxHeight: '100%'}}>
+                        <Markdown gfm={true} breaks={true}>
+                            {this.state.preview}
+                        </Markdown>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ 3-frontend-dev-libraries/2-markdown-previewer/src/defaultPreview.js
@@ -0,0 +1,46 @@
+const defaultPreview =
+    `
+# Markdown preview!!!! yay!!!!!
+
+## soooo many things to do here
+### some examples:
+
+code, y'all know I like code: \`<div id="blabla"></div>\`
+
+\`\`\`
+// Code, but many lineeeees
+
+function blabla() {
+  while (true) {
+    console.log('FreeCodeCamp is awesome!');
+  }
+}
+\`\`\`
+
+Pretty **bold**... claim tbh.
+
+Would be a shame if something ~~happened to it~~.
+
+Please look at my [GitHub](https://github.com/CherryKitten) profile!
+
+> Block Quotes!
+> WHAT
+> WHY
+> OKAY
+
+
+- Things I like:
+    - Cats
+        - Black ones
+        - Orange ones
+        - Grey ones
+        - White ones
+        - Striped ones
+        - Dotted ones
+        - And all those that I forgot
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+`
+
+export default defaultPreview
